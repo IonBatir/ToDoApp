28f6cacca2f3ff8ea80c8808b2c0b1ba290fe260
Ion Batîr <ionbatir@gmail.com>
2019-04-20 14:26:25 +0300

    Add complete property to ToDo

--- app/src/main/java/com/example/todoapp/ToDo.java
+++ app/src/main/java/com/example/todoapp/ToDo.java
@@ -8,26 +8,36 @@ public class ToDo {
     private String uuid;
     private String title;
     private Date date;
+    private boolean completed;
 
     public ToDo(String title, Date date) {
         uuid = UUID.randomUUID().toString();
         this.title = title;
         this.date = date;
+        this.completed = false;
     }
 
-    String getUuid() {
+    public String getUuid() {
         return uuid;
     }
 
-    String getTitle() {
+    public String getTitle() {
         return title;
     }
 
-    Date getDate() {
+    public Date getDate() {
         return date;
     }
 
-    String getFormattedDate() {
+    public String getFormattedDate() {
         return DateFormat.getDateInstance().format(date);
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void toggleComplete() {
+        completed = !completed;
     }
 }
